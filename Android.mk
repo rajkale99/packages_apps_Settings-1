99b334c946bc6fee5ce83289f81fa6de1958adf0
rohan <purohit.rohan@gmail.com>
2020-07-03 04:48:19 -0400

    Settings: import airbnb-lottie
    Change-Id: I4f2c38ff15a6bb663379d1aae551748c42688630
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- Android.mk
+++ Android.mk
@@ -36,6 +36,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.recyclerview_recyclerview \
     com.google.android.material_material \
     setupcompat \
+    airbnb-lottie \
     setupdesign
 
 LOCAL_JAVA_LIBRARIES := \
@@ -52,8 +53,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     settings-log-bridge-protos-lite \
     contextualcards \
     settings-logtags \
-    zxing-core-1.7
-	
+    zxing-core-1.7 \
+    airbnb-lottie
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     packages/apps/Legion/res \
     external/google/SettingsGoogle/res
