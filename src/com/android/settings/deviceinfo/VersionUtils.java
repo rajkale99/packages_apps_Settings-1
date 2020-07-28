274f3296b322bda3ea7da356021320dd893e951e
rajkale99 <kaleraj.rk@gmail.com>
2020-07-28 10:36:34 +0000

    Fix VersionUtil

--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -5,7 +5,7 @@ import android.os.SystemProperties;
 
 public class VersionUtils {
     public static String getLegionVersion(){
-        String buildType = SystemProperties.get("ro.legion.build.version","");
+        String buildType = SystemProperties.get("ro.legion.version","");
 	return buildType.equals("") ? "" : buildType;
     }
 }
