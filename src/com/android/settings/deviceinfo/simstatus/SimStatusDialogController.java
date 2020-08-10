Evokkimus/android_packages_apps_Settings

show4GForLTE Fix
Credit to jereksel for guidance with the commit below
https://gerrit.dirtyunicorns.com/#/c/5550/

Change-Id: If9beed59df6e4319b6a6e9d84eafbfe2772aad19

src/com/android/settings/deviceinfo/simstatus/SimStatusDialogController.java
@@ -25,11 +25,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.telephony.CarrierConfigManager;
@@ -44,7 +44,6 @@ import android.telephony.euicc.EuiccManager;
 import android.text.BidiFormatter;
 import android.text.TextDirectionHeuristics;
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settingslib.DeviceInfoUtils;
@@ -336,18 +335,10 @@ public class SimStatusDialogController implements LifecycleObserver, OnResume, O
             voiceNetworkTypeName = mTelephonyManager.getNetworkTypeName(actualVoiceNetworkType);
         }
 
-        boolean show4GForLTE = false;
-        try {
-            final Context con = mContext.createPackageContext(
-                    "com.android.systemui", 0 /* flags */);
-            final int id = con.getResources().getIdentifier("config_show4GForLTE",
-                    "bool" /* default type */, "com.android.systemui" /* default package */);
-            show4GForLTE = con.getResources().getBoolean(id);
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.e(TAG, "NameNotFoundException for show4GForLTE");
-        }
+        boolean mShow4GForLTE = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SHOW_LTE_FOURGEE, 0, UserHandle.USER_CURRENT) == 1;
 
-        if (show4GForLTE) {
+        if (mShow4GForLTE) {
             if ("LTE".equals(dataNetworkTypeName)) {
                 dataNetworkTypeName = "4G";
             }

rajkale99/packages_apps_Settings-1

Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

src/com/android/settings/deviceinfo/simstatus/SimStatusDialogController.java
@@ -30,6 +30,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.telephony.CarrierConfigManager;
 import android.telephony.CellBroadcastMessage;
 import android.telephony.PhoneStateListener;
@@ -382,14 +383,10 @@ public class SimStatusDialogController implements LifecycleObserver, OnResume, O
             voiceNetworkTypeName = mTelephonyManager.getNetworkTypeName(actualVoiceNetworkType);
         }
 
-        boolean show4GForLTE = false;
-        final PersistableBundle carrierConfig = mCarrierConfigManager.getConfigForSubId(subId);
-        if (carrierConfig != null) {
-            show4GForLTE = carrierConfig.getBoolean(
-                    CarrierConfigManager.KEY_SHOW_4G_FOR_LTE_DATA_ICON_BOOL);
-        }
+        boolean mShow4GForLTE = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SHOW_FOURG_ICON, 0, UserHandle.USER_CURRENT) == 1;
 
-        if (show4GForLTE) {
+        if (mShow4GForLTE) {
             if ("LTE".equals(dataNetworkTypeName)) {
                 dataNetworkTypeName = "4G";
             }